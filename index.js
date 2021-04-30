0cdb2319031cf30389dd255a8509e185288fcea5
anissa guembre <guembreani@gmail.com>
2021-04-30 05:07:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun (activity = 'roller-skate', clean = "bathe my dog!"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+function mondayWork( workOffice = 'go to the office',worHome= "work from home"){
+    return(`This Monday, I will ${workOffice}.`);
+
+}
+function wrapAdjective(wrap = "*"){
+    return function adjective(adj='noun') {
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+
+}
